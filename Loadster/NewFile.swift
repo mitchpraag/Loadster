75147a5980fdef3ab8f836cf7e4021166dce76db
mitchpraag <mitchell.praag@gmail.com>
2017-08-31 18:00:21 -0600

    Again

--- Loadster/NewFile.swift
+++
@@ -1,9 +0,0 @@
-//
-//  NewFile.swift
-//  Loadster
-//
-//  Created by Mitch Praag on 8/31/17.
-//  Copyright © 2017 Mitch Praag. All rights reserved.
-//
-
-import Foundation
--- Loadster/Test 2.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Test 2.swift
-//  Loadster
-//
-//  Created by Mitch Praag on 8/31/17.
-//  Copyright © 2017 Mitch Praag. All rights reserved.
-//
-
-import Foundation
